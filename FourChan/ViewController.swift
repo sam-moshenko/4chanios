9fe146eedff620fb160e704b49f8e9c13666ffd9
Lebedev Semen <lebedev.semen@kcell.kz>
2023-06-19 15:13:19 +0600

    feat: create project

---
+++ FourChan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FourChan
+//
+//  Created by Semen Lebedev on 19.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
