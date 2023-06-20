3b220531ee9ae0885c0aeb03e1c012748f081308
Akmaral <e.akmaral024@mail.ru>
2023-06-20 16:03:33 +0600

    Complete tasks

--- FourChan/Modules/Threads/Models/ThreadsViewModel.swift
+++ FourChan/Modules/Threads/Models/ThreadsViewModel.swift
@@ -1,22 +1,58 @@
 import Foundation
 
 struct ThreadsViewModel {
-    var board: Board
+    var board: Board?
     var cells: [CellModel]
+    var isChoosingBoard: Bool = false
     
     enum Board: String, CaseIterable {
-        case a, v, mu, gd
+        case a, v, mu, gd, fit
         
         var description: String {
-            "/\(rawValue)/"
+            let boardURLString = "https://boards.4channel.org/\(rawValue)/"
+            guard let boardURL = URL(string: boardURLString) else {
+                return "/\(rawValue)/"
+            }
+            
+            let semaphore = DispatchSemaphore(value: 0)
+            var description: String = "/\(rawValue)/"
+            
+            URLSession.shared.dataTask(with: boardURL) { (data, response, error) in
+                defer { semaphore.signal() }
+                
+                if let error = error {
+                    print("Error retrieving board description: \(error)")
+                    return
+                }
+                
+                guard let data = data else {
+                    print("No data received for board description")
+                    return
+                }
+                
+                if let html = String(data: data, encoding: .utf8),
+                   let startRange = html.range(of: "<title>"),
+                   let endRange = html.range(of: "</title>") {
+                    let titleRange = startRange.upperBound..<endRange.lowerBound
+                    let title = html[titleRange].replacingOccurrences(of: " - 4chan", with: "")
+                    description = "/\(self.rawValue)/ - \(title)"
+                }
+            }.resume()
+            
+            _ = semaphore.wait(timeout: .distantFuture)
+            
+            return description
         }
     }
+    
     
     struct CellModel {
         var id: Int
         var title: String?
         var description: String?
         var imageUrl: URL?
+        var creationDate: Date?
+        var username: String?
         
         init?(data: ThreadResponse.Post, board: String) {
             if data.sub == nil && data.com == nil && data.tim == nil { return nil }
@@ -26,6 +62,23 @@ struct ThreadsViewModel {
             if let tim = data.tim, let ext = data.ext {
                 imageUrl = URL(string: "https://i.4cdn.org/\(board)/\(tim)\(ext)")!
             }
+            // Set the creation date and username properties
+//            if let timestamp = data.time {
+//                creationDate = Date(timeIntervalSince1970: TimeInterval(timestamp))
+//            }
+//            username = data.username // Update this line to use the correct property name for username
+        }
+    
+        //
+        func formattedCreationDate() -> String? {
+            guard let creationDate = creationDate else {
+                return nil
+            }
+            // Format the creation date as desired
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateStyle = .short
+            dateFormatter.timeStyle = .short
+            return dateFormatter.string(from: creationDate)
         }
     }
 }
--- FourChan/Modules/Threads/View/ThreadsView.swift
+++ FourChan/Modules/Threads/View/ThreadsView.swift
@@ -3,19 +3,37 @@ import SnapKit
 
 protocol ThreadsViewDelegate: AnyObject {
     func boardButtonTapped()
+    func cancelButtonTapped()
     func didSelectItem(_ item: ThreadsViewModel.CellModel)
+   
 }
 
-class ThreadsView: BaseView {
+class ThreadsView: BaseView, UITableViewDelegate {
+    
     lazy var stackView: UIStackView = build {
         $0.axis = .vertical
         $0.spacing = 8
-        $0.addArrangedSubview(boardButton)
+        $0.addArrangedSubview(boardButtonStackView)
         $0.addArrangedSubview(tableView)
+    }
+    
+    lazy var boardButtonStackView: UIStackView = build {
+        $0.axis = .horizontal
+        $0.spacing = 8
+        $0.addArrangedSubview(boardButton)
+        $0.addArrangedSubview(cancelButton)
     }
     
     lazy var boardButton: UIButton = build(.init(type: .roundedRect)) {
         $0.addTarget(self, action: #selector(boardButtonTapped), for: .touchUpInside)
+        $0.titleLabel?.numberOfLines = 2
+        $0.titleLabel?.lineBreakMode = .byTruncatingTail
+    }
+    
+    lazy var cancelButton: UIButton = build(.init(type: .roundedRect)) {
+        $0.addTarget(self, action: #selector(cancelButtonTapped), for: .touchUpInside)
+        $0.setTitle(NSLocalizedString("Cancel", comment: ""), for: .normal)
+        
     }
     
     lazy var tableView: UITableView = build {
@@ -25,8 +43,8 @@ class ThreadsView: BaseView {
     }
     
     weak var delegate: ThreadsViewDelegate?
-    
     private let dataSource: ThreadsTableViewDatasource = .init()
+    private var isChoosingBoard: Bool = false
     
     override func setup() {
         addSubview(stackView) { $0.edges.equalToSuperview().inset(8) }
@@ -35,17 +53,26 @@ class ThreadsView: BaseView {
     func configure(_ viewModel: ThreadsViewModel) {
         dataSource.items = viewModel.cells
         tableView.reloadData()
-        boardButton.setTitle(viewModel.board.description, for: .normal)
+        
+        if isChoosingBoard {
+            boardButtonStackView.isHidden = true
+        } else {
+            boardButtonStackView.isHidden = false
+            boardButton.setTitle(viewModel.board?.description, for: .normal)
+        }
     }
     
-    @objc
-    func boardButtonTapped() {
+    @objc func boardButtonTapped() {
+        isChoosingBoard = true
+        boardButtonStackView.isHidden = true
         delegate?.boardButtonTapped()
     }
-}
-
-extension ThreadsView: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        delegate?.didSelectItem(dataSource.items[indexPath.row])
+    
+    @objc func cancelButtonTapped() {
+        isChoosingBoard = false
+        boardButtonStackView.isHidden = false
+        delegate?.cancelButtonTapped()
     }
+    
+    
 }
